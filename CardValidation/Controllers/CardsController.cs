530b0a10ae339d61ce156917e0e5dc9396cd52c4
Serbruyns Matthias <9572545+mserbruyns@users.noreply.github.com>
2018-10-09 22:10:31 +0200

    add cards controller

---
+++ CardValidation/Controllers/CardsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CardValidation.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CardValidation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly CardValidationContext _context;
+
+        public CardsController(CardValidationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cards
+        [HttpGet]
+        public IEnumerable<Card> GetAllCards()
+        {
+            return _context.Cards;
+        }
+
+
+        [HttpGet("validatecard")]
+        public IEnumerable<Card> ValidateCard(string cardNumber, string expiryDate)
+        {
+
+            return _context.Cards;
+        }
+
+    }
+}
