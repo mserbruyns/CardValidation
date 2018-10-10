0ffa7f243dd4ce1c59b1e13e8448f7f8cf3115f4
Matthias Serbruyns <9572545+mserbruyns@users.noreply.github.com>
2018-10-10 16:51:18 +0200

    CardValidationRepository

---
+++ CardValidation/Core/Interfaces/ICardValidationRepository.cs
@@ -0,0 +1,14 @@
+﻿using CardValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardValidation.Core.Interfaces
+{
+    public interface ICardValidationRepository
+    {
+        ValidationResponse ValidateCard(string cardNumber, string cardType, string expiryDate);
+        Task AddCard(Card card);
+    }
+}
---
+++ CardValidation/Infrastructure/CardValidationRepository.cs
@@ -0,0 +1,75 @@
+﻿using CardValidation.Core.Interfaces;
+using CardValidation.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardValidation.Infrastructure
+{
+    public class CardValidationRepository : ICardValidationRepository
+    {
+        private readonly CardValidationContext _dbContext;
+
+        public CardValidationRepository(CardValidationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task AddCard(Card card)
+        {
+            _dbContext.Cards.Add(card);
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public ValidationResponse ValidateCard(string cardNumber, string cardType, string expiryDate)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>()
+            {
+                 new SqlParameter
+                 {
+                     ParameterName = "@cardNumber",
+                     DbType = System.Data.DbType.String,
+                     Direction = System.Data.ParameterDirection.Input,
+                     Value = cardNumber
+                 },
+                 new SqlParameter
+                 {
+                    ParameterName = "@cardType",
+                    DbType = System.Data.DbType.String,
+                    Direction = System.Data.ParameterDirection.Input,
+                    Value = cardType
+                 },
+                 new SqlParameter
+                 {
+                    ParameterName = "@input_expiry",
+                    DbType = System.Data.DbType.String,
+                    Direction = System.Data.ParameterDirection.Input,
+                    Value = expiryDate
+                 }
+            };
+
+            var outputParameter = new SqlParameter("@result", DbType.String)
+            {
+                Direction = ParameterDirection.Output,
+                DbType = System.Data.DbType.String,
+                Size = 50
+
+            };
+            parameters.Add(outputParameter);
+            _dbContext.Database.ExecuteSqlCommand("exec GetcardinformationTest @cardNumber, @cardType, @input_expiry, @result OUT", parameters);
+
+            var response = new ValidationResponse()
+            {
+                CardType = cardType,
+                CardStatus = outputParameter.Value.ToString()
+
+            };
+
+            return response;
+        }
+    }
+}
