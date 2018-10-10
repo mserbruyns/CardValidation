2c9c4e7f1ecf87264e6a480fcc3a41c4cee7642a
Matthias Serbruyns <9572545+mserbruyns@users.noreply.github.com>
2018-10-10 16:54:49 +0200

    StartUp config
    DependencyInjection CardValidationRepository
    Swagger

--- CardValidation/Startup.cs
+++ CardValidation/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CardValidation.Core.Interfaces;
+using CardValidation.Infrastructure;
 using CardValidation.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CardValidation
 {
@@ -27,15 +30,36 @@ namespace CardValidation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
             var connection = Configuration.GetConnectionString("CardValidationContext");
             services.AddDbContext<CardValidationContext>(options => options.UseSqlServer(connection));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+           // services.AddScoped<ICardValidationRepository, CardValidationRepository>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Validation API", Version = "v1" });
+            });
+
+            // Register application services.
+            services.AddScoped<ICardValidationRepository, CardValidationRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Validation API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
