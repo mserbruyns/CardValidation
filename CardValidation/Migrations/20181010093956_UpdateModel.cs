a0872dc8f0d48c20853e23314786c3b84a4cf616
Matthias Serbruyns <9572545+mserbruyns@users.noreply.github.com>
2018-10-10 16:49:54 +0200

    Migrations

---
+++ CardValidation/Migrations/20181010093956_UpdateModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CardValidation.Migrations
+{
+    public partial class UpdateModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "expiryDate",
+                table: "Cards",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "expiryDate",
+                value: "102004");
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "expiryDate",
+                value: "102004");
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "expiryDate",
+                value: "102004");
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 4,
+                column: "expiryDate",
+                value: "102004");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "expiryDate",
+                table: "Cards",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "expiryDate",
+                value: new DateTime(2004, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "expiryDate",
+                value: new DateTime(2004, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 3,
+                column: "expiryDate",
+                value: new DateTime(2004, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "id",
+                keyValue: 4,
+                column: "expiryDate",
+                value: new DateTime(2004, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
