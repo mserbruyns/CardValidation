fe1685cf42d344e3f37c9209120de79557b71e9e
Matthias Serbruyns <9572545+mserbruyns@users.noreply.github.com>
2018-10-10 16:55:03 +0200

    RegexUtils

---
+++ CardValidation/Utils/RegexUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CardValidation.Utils
+{
+    public static class RegexUtils
+    {
+        private static readonly Regex rxNonDigits = new Regex(@"[^\d]+");
+
+        public static string OnlyDigits(string value)
+        {
+            value = rxNonDigits.Replace(value, "");
+            return value;
+        }
+    }
+}
